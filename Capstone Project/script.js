391286e8f0d9119e80e628c5dd9e5120524e4be5
Raaghavi-K-R <99703058+Raaghavi-K-R@users.noreply.github.com>
2022-12-11 19:58:51 +0530

    Add files via upload

---
+++ Capstone Project/script.js
@@ -0,0 +1,40 @@
+let menu = document.querySelector('#menu-bar');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () =>{
+
+  menu.classList.toggle('fa-times');
+  navbar.classList.toggle('active');
+
+}
+
+window.onscroll = () =>{
+
+  menu.classList.remove('fa-times');
+  navbar.classList.remove('active');
+
+  if(window.scrollY > 60){
+    document.querySelector('#scroll-top').classList.add('active');
+  }else{
+    document.querySelector('#scroll-top').classList.remove('active');
+  }
+
+}
+
+function loader(){
+  document.querySelector('.loader-container').classList.add('fade-out');
+}
+
+function fadeOut(){
+  setInterval(loader, 1000);
+}
+
+window.onload = fadeOut();
+
+function FbotonOn() { 
+
+  if(document.getElementById('checkBox').checked)
+      document.getElementById('texto').innerHTML = "Thank you!Your order has been placed successfully.";
+  else
+      document.getElementById('texto').innerHTML = "Something went wrong. Please try again!";
+}
